00d527e5d5dffae851bfddfa50ca63c55d19957f
Rogério Marques <rogerrm@live.com>
2024-04-08 20:48:45 -0300

    Páginas e componentes (UI) - ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
-    <HelmetProvider>
-      <Toaster richColors />
-      <RouterProvider router={router} />
-      <Helmet titleTemplate="%s | pizza.shop" />
-    </HelmetProvider>
+    <ThemeProvider storageKey="pizzashop-theme">
+      <HelmetProvider>
+        <Toaster richColors />
+        <RouterProvider router={router} />
+        <Helmet titleTemplate="%s | pizza.shop" />
+      </HelmetProvider>
+    </ThemeProvider>
   )
 }
