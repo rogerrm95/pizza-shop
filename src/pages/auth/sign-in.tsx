c5f22ee053ba3f5c57a91d1317811b250871bc89
Rogério Marques <rogerrm@live.com>
2024-04-16 20:45:37 -0300

    Conexão com API - Autenticação do usuário

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,9 +1,11 @@
+import { useMutation } from '@tanstack/react-query'
 import { Helmet } from 'react-helmet-async'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom'
 import { toast } from 'sonner'
 import { z } from 'zod'
 
+import { signIn } from '@/api/sign-in'
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
@@ -21,13 +23,20 @@ export function SignIn() {
     formState: { isSubmitting },
   } = useForm<SignInForm>()
 
+  const { mutateAsync: authenticate } = useMutation({ mutationFn: signIn })
+
   async function handleSignIn(data: SignInForm) {
     try {
-      await new Promise((resolve) => setTimeout(resolve, 5000))
-      toast.success(`Um e-mail foi enviado contendo o link para autenticação!`)
-      console.log(data)
+      await authenticate({ email: data.email })
+
+      toast.success('Enviamos um link de autenticação para seu e-mail', {
+        action: {
+          label: 'Reenviar',
+          onClick: () => handleSignIn(data),
+        },
+      })
     } catch {
-      toast.error(`Credenciais inválidas!`)
+      toast.error('Credenciais inválidas!')
     }
   }
 
