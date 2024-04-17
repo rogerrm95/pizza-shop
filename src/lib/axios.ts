3fff444cdf5f5b5b54e8adf90b3dd116ac4365f3
Rogério Marques <rogerrm@live.com>
2024-04-16 22:59:23 -0300

    Conexão com API - Loading state no perfil

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -6,3 +6,10 @@ export const api = axios.create({
   baseURL: env.VITE_API_URL,
   withCredentials: true, // Permitir que o Back-End tenha acesso aos Cookies //
 })
+
+if (env.VITE_ENABLE_API_DELAY) {
+  api.interceptors.request.use(async (config) => {
+    await new Promise((resolve) => setTimeout(resolve, 1500))
+    return config
+  })
+}
