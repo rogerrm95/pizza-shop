612305218cec12a03f024fd9e9afc8830c1d3c28
Rogério Marques <rogerrm@live.com>
2024-04-17 20:53:23 -0300

    Conexão com API - Atualizando perfil

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,6 +16,8 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        success:
+          'bg-emerald-500 text-white hover:bg-emerald-500/90 dark:bg-emerald-600 dark:hover:emerald-600/90',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
