84dffa35317c6bd8716613a24a1da70f3c1c960d
Rogério Marques <rogerrm@live.com>
2024-05-13 21:49:01 -0300

    Testes e mocks - Teste unitário: Order Status

--- src/components/order-status.tsx
+++ src/components/order-status.tsx
@@ -13,27 +13,39 @@ const orderStatusMap: Record<OrderStatus, string> = {
   pending: 'Pendente',
   canceled: 'Cancelado',
   delivered: 'Entregue',
-  delivering: 'Em preparo',
-  processing: 'Em processo',
+  delivering: 'Em entrega',
+  processing: 'Em preparo',
 }
 
 export function OrderStatus({ status }: OrderStatusProps) {
   return (
     <div className="flex items-center gap-2">
       {status === 'pending' && (
-        <span className="h-2 w-2 rounded-full bg-slate-400" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-slate-400"
+        />
       )}
 
       {status === 'canceled' && (
-        <span className="h-2 w-2 rounded-full bg-rose-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-rose-500"
+        />
       )}
 
       {status === 'delivered' && (
-        <span className="h-2 w-2 rounded-full bg-emerald-500" />
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-emerald-500"
+        />
       )}
 
-      {['delivering', 'processing'].includes(status) && (
-        <span className="h-2 w-2 rounded-full bg-amber-500" />
+      {['processing', 'delivering'].includes(status) && (
+        <span
+          data-testid="badge"
+          className="h-2 w-2 rounded-full bg-amber-500"
+        />
       )}
 
       <span className="font-medium text-muted-foreground">
