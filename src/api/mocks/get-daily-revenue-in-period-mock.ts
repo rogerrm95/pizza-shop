89e607a31609fa8cff72231d028b6dfac5c395ec
Rogério Marques <rogerrm@live.com>
2024-05-23 21:15:34 -0300

    add: Adiciona Mock para o Date Picker

--- src/api/mocks/get-daily-revenue-in-period-mock.ts
+++ src/api/mocks/get-daily-revenue-in-period-mock.ts
@@ -1,17 +1,52 @@
+import { addDays, differenceInDays, format } from 'date-fns'
+import { ptBR } from 'date-fns/locale'
 import { http, HttpResponse } from 'msw'
 
-import { GetDailyRevenueInPeriodResponse } from '../get-daily-revenue-in-period'
+import type { GetDailyRevenueInPeriodResponse } from '../get-daily-revenue-in-period'
 
 export const getDailyRevenueInPeriodMock = http.get<
   never,
   never,
   GetDailyRevenueInPeriodResponse
->('/metrics/daily-receipt-in-period', () => {
-  return HttpResponse.json([
-    { date: '01/01/2024', receipt: 100 },
-    { date: '02/01/2024', receipt: 200 },
-    { date: '03/01/2024', receipt: 300 },
-    { date: '04/01/2024', receipt: 400 },
-    { date: '05/01/2024', receipt: 500 },
-  ])
+>('/metrics/daily-receipt-in-period', ({ request }) => {
+  const { searchParams } = new URL(request.url)
+
+  const from = searchParams.get('from')
+  const to = searchParams.get('to')
+
+  // VERIFICA SE HÁ AMBOS OS PARAMETROS //
+  // SE HOUVER //
+  if (from && to) {
+    // Calcula a diferença entre os dias //
+    const differenceInDaysInPeriod = differenceInDays(
+      new Date(to),
+      new Date(from),
+    )
+
+    const dailyRevenueInPeriodData: GetDailyRevenueInPeriodResponse =
+      Array.from({
+        length:
+          differenceInDaysInPeriod >= 7 ? 7 : differenceInDaysInPeriod + 1,
+      }).map((_, index) => {
+        return {
+          date: format(
+            addDays(new Date(from), index).toISOString(),
+            'dd/MM/yyyy',
+            { locale: ptBR },
+          ),
+          receipt: 1000 * (Math.random() * index),
+        }
+      })
+
+    return HttpResponse.json(dailyRevenueInPeriodData)
+  }
+  // SENÃO //
+  else {
+    return HttpResponse.json([
+      {
+        date: format(new Date(), 'dd/MM/yyyy', { locale: ptBR }),
+        receipt: 0,
+      },
+    ])
+  }
 })
