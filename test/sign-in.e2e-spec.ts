edccdbc5ec6f68ccd4ecc45c5be3c889b220938e
Rogério Marques <rogerrm@live.com>
2024-06-04 20:32:01 -0300

    Testes e mocks - Teste E2E: Sign In

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,37 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('rogerrm2014@gmail.com')
+
+  await page.getByRole('button', { name: 'Acessar Painel' }).click()
+
+  const toast = page.getByText(
+    'Enviamos um link de autenticação para seu e-mail',
+  )
+
+  await expect(toast).toBeVisible()
+
+  // await page.waitForTimeout(1000)
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Seu e-mail').fill('wrong@test.com')
+
+  await page.getByRole('button', { name: 'Acessar Painel' }).click()
+
+  const toast = page.getByText('Credenciais inválidas!')
+
+  await expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'Novo estabelecimento' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
